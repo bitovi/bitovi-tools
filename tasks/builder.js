bb51514396f200249079b8e1f88eec854408a580
Stan Carrico <shcarrico@gmail.com>
2013-05-28 01:33:38 -0400

    builder supports plugins

--- tasks/builder.js
+++ tasks/builder.js
@@ -3,33 +3,71 @@ var path = require('path');
 var steal = require('steal');
 var builder = steal.build.builder;
 
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 	var _ = grunt.util._;
-
-	grunt.registerMultiTask('builder', 'Pluginify using the download builder configuration', function() {
+	grunt.registerMultiTask('builder', 'Pluginify using the download builder configuration', function () {
 		var done = this.async();
 		var options = this.options();
-		this.files.forEach(function(f) {
+		this.files.forEach(function (f) {
 			grunt.file.mkdir(f.dest);
-			f.src.forEach(function(src) {
+			var plugins = _.filter(_.keys(options.builder.modules), function (mod) {
+				return options.builder.modules[mod]["type"] === "plugin";
+			});
+
+			f.src.forEach(function (src) {
 				builder({
 					path: fs.realpathSync(src),
-					steal: options.steal,
-				}, function(error, info, build) {
+					steal: options.steal
+				}, function (error, info, build) {
+
 					var configurations = options.configurations || _.keys(info.configurations);
-					configurations.forEach(function(name) {
-						var builderOptions = {
-							configuration: name,
-							url: options.url,
-							pluginify: options.pluginify
-						};
-						build(builderOptions, function(error, content) {
+
+					var builderOptions = {
+						generatedPlugins: {},
+						url: options.url,
+						pluginify: options.pluginify,
+						plugins: plugins
+					};
+
+					//build core distributable
+					configurations.forEach(function (name) {
+
+						builderOptions.configuration = name;
+						delete builderOptions.plugin;
+
+						build(builderOptions, function (error, content, banner) {
 							var filename = path.join(f.dest, (options.prefix || '') + name + '.js');
 							console.log('Writing ' + filename);
-							grunt.file.write(filename, content);
-							done();
+							grunt.file.write(filename, banner + content);
 						});
+
+						plugins.forEach(function (plugin) {
+							var pluginConfigs = options.builder.modules[plugin].configurations;
+							builderOptions.plugin = plugin;
+
+							if (!builderOptions.generatedPlugins[plugin]) {
+
+
+								if (pluginConfigs) {
+									if (pluginConfigs.indexOf(name) === -1) {
+										return false;
+									}
+								}
+
+								builderOptions.generatedPlugins[plugin] = true;
+
+								build(builderOptions, function (error, content, banner) {
+									var filename = path.join(f.dest, options.builder.modules[plugin].name.toLowerCase() + '.js');
+									console.log('Writing ' + filename);
+									grunt.file.write(filename, banner + content);
+								});
+							}
+
+						});
+
 					});
+					done();
+
 				});
 			});
 		});
