ca6f5ce87f86b00340100640e723fabf2de6b627
David Luecke <daff@neyeon.com>
2013-11-07 13:40:31 -0700

    Updating changelog task. Retrieve last closed milestone automatically and prevent duplicate changelogs.

--- tasks/changelog.js
+++ tasks/changelog.js
@@ -1,68 +1,80 @@
-var https = require('https'),
-querystring = require('querystring'),
-ejs = require('ejs');
+var https = require('https');
+var querystring = require('querystring');
+var ejs = require('ejs');
+var request = require('request');
+var githubApi = 'https://api.github.com';
+
+function getIssues(options, callback) {
+  var params = querystring.stringify({
+    milestone: options.milestone,
+    state: 'closed',
+    per_page: 100
+  });
+  var path = '/repos/' + options.user + '/' + options.repo + '/issues?' + params;
+
+  request({
+    url: githubApi + path,
+    json: true
+  }, function(error, data) {
+    callback(error, data.body);
+  });
+}
+
+function getMilestone(options, callback) {
+  if (options.milestone) {
+    return callback(null, options.milestone);
+  }
+
+  var params = querystring.stringify({
+    state: 'closed',
+    sort: 'due_date',
+    direction: 'desc'
+  });
+  var path = '/repos/' + options.user + '/' + options.repo + '/milestones?' + params;
+
+  request({
+    url: githubApi + path,
+    json: true
+  }, function(error, data) {
+    if(data.body && data.body[0] && data.body[0]) {
+      return callback(error, data.body[0]);
+    }
+
+    callback(new Error('No milestone number found'));
+  });
+}
 
 module.exports = function(grunt) {
+  grunt.registerTask('changelog', 'Updates changelog.md based on GitHub milestones', function() {
+    var done = this.async();
+    var options = this.options();
 
-	grunt.registerMultiTask('changelog', 'Updates changelog.md based on GitHub milestones', function() {
-		var done = this.async(),
+    getMilestone(options, function(error, milestone) {
+      grunt.log.writeln('Reading issues for milestone ' + milestone.title + ' (#' + milestone.number + ')');
+      options.milestone = milestone.number;
+      getIssues(options, function(error, issues) {
+        if (grunt.file.exists('changelog.md')) {
+          log = grunt.file.read('changelog.md');
+        }
 
-		params = querystring.stringify({
-			milestone: this.data.milestone,
-			state: 'closed',
-			per_page: 100
-		}),
+        ejs.renderFile(__dirname + '/../resources/changelog.ejs', {
+          version: options.version,
+          date: new Date(Date.now()),
+          issues: issues
+        }, function(e, template) {
+          if (e) {
+            return done(e);
+          }
 
-		path = '/repos/' + this.data.user + '/' + this.data.repo + '/issues?' + params,
+          if(log.indexOf(template) !== -1) {
+            grunt.log.error('Changelog entry already exists.');
+          } else {
+            grunt.file.write('changelog.md', log.replace('-->', '-->\n' + template));
+          }
 
-		buffer = '',
-		self = this;
-
-		var write = function() {
-			var issues = JSON.parse(buffer),
-			log = '';
-
-			if(grunt.file.exists('changelog.md')) {
-				log = grunt.file.read('changelog.md');
-			}
-
-			ejs.renderFile(__dirname + '/../resources/changelog.ejs', {
-				version: self.data.version,
-				date: new Date(Date.now()),
-				issues: issues
-			}, function(e, template) {
-
-				if(e) {
-					done(e);
-				}
-
-				grunt.file.write('changelog.md', template + log);
-				done();
-
-			});
-		},
-
-		req = https.request({
-			hostname: 'api.github.com',
-			path: path,
-			headers: {
-				'User-Agent': 'node.js'
-			}
-		}, function(res) {
-
-			res.on('data', function(data) {
-				buffer += data;
-			});
-
-			res.on('end', write);
-		});
-
-		req.end();
-
-		req.on('error', function(e) {
-			done(e);
-		});
-
-	});
-
+          done();
+        });
+      });
+    });
+  });
 }
