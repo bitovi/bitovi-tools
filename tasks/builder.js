c0b2ecea6daae8bbade06e809d65fe5cc619c98d
David Luecke <daff@neyeon.com>
2014-03-11 16:10:00 -0600

    Updating builder Grunt task to include non-core dependencies when building a plugin.

--- tasks/builder.js
+++ tasks/builder.js
@@ -6,13 +6,16 @@ var builder = steal.build.builder;
 module.exports = function (grunt) {
     var _ = grunt.util._;
 
-    function buildFiles(build, name, dest, dev, options) {
+    function buildFiles(build, name, dest, dev, options, callback) {
         name = path.join(dest, name);
 
-        build(_.omit(options, 'dev'), function (error, content, banner) {
+        build(_.omit(options, 'dev'), function (error, content, banner, steals) {
             var filename = name + '.js';
             console.log('Writing ' + filename);
             grunt.file.write(filename, banner + content);
+            if (callback) {
+                callback.apply(this, arguments);
+            }
         });
 
         if (dev) {
@@ -28,60 +31,59 @@ module.exports = function (grunt) {
         var done = this.async();
         var options = this.options();
         this.files.forEach(function (f) {
-            grunt.file.mkdir(f.dest);
-            var plugins = _.filter(_.keys(options.builder.modules), function (mod) {
-                return options.builder.modules[mod]["type"] === "plugin" && !options.builder.modules[mod].hidden;
-            });
+            var src = f.src[0];
 
-            f.src.forEach(function (src) {
-                builder({
-                    path: fs.realpathSync(src),
-                    steal: options.steal
-                }, function (error, info, build) {
+            builder({
+                path: fs.realpathSync(src),
+                steal: options.steal
+            }, function (error, info, build) {
+                var configurations = options.configurations || _.keys(info.configurations);
+                var defaultConfiguration = configurations.shift();
+                var builderOptions = {
+                    url: options.url,
+                    dev: options.dev,
+                    pluginify: options.pluginify
+                };
+                var buildWithConfiguration = function (name, callback) {
+                    if (info.configurations[name].hidden) {
+                        return;
+                    }
 
-                    var configurations = options.configurations || _.keys(info.configurations);
+                    builderOptions.configuration = name;
 
-                    var builderOptions = {
-                        generatedPlugins: {},
-                        url: options.url,
-                        dev: options.dev,
-                        pluginify: options.pluginify,
-                        plugins: plugins
-                    };
+                    buildFiles(build, (options.prefix || '') + name, f.dest, options.dev, builderOptions, callback);
+                }
 
-                    //build core distributable
-                    configurations.forEach(function (name) {
-                        if (info.configurations[name].hidden) {
-                            return;
-                        }
-
-                        builderOptions.configuration = name;
-                        delete builderOptions.plugin;
-
-                        buildFiles(build, (options.prefix || '') + name, f.dest, options.dev, builderOptions);
-
-                        plugins.forEach(function (plugin) {
-                            var pluginConfigs = options.builder.modules[plugin].configurations;
-                            builderOptions.plugin = plugin;
-
-                            if (!builderOptions.generatedPlugins[plugin]) {
-
-
-                                if (pluginConfigs && pluginConfigs.indexOf(name) === -1) {
-                                    return false;
-                                }
-
-                                builderOptions.generatedPlugins[plugin] = true;
-
-                                buildFiles(build, options.builder.modules[plugin].name.toLowerCase(), f.dest, false, builderOptions);
-                            }
-
-                        });
-
-                    });
-                    done();
-
+                // build core for other libraries
+                configurations.forEach(function(name) {
+                    buildWithConfiguration(name);
                 });
+
+                // Build the default configuration and all plugins (excluding the core Steals)
+                buildWithConfiguration(defaultConfiguration, function (error, content, banner, steals) {
+                    var plugins = _.filter(_.keys(options.builder.modules), function (mod) {
+                        return options.builder.modules[mod]["type"] === "plugin" && !options.builder.modules[mod].hidden;
+                    });
+
+                    // Ignore the core steals
+                    builderOptions.pluginify.ignore = steals.map(function (stl) {
+                        return '' + stl.options.id;
+                    });
+                    builderOptions.pluginify.shim = _.extend({
+                        "can/util/util.js": "window.can"
+                    }, info.pluginify.shim);
+                    builderOptions.pluginify.exports = {};
+
+                    // Build all plugins
+                    plugins.forEach(function (plugin) {
+                        var name = options.builder.modules[plugin].name.toLowerCase();
+
+                        builderOptions.ids = [plugin];
+                        buildFiles(build, name, f.dest, false, builderOptions);
+                    });
+                });
+
+                done();
             });
         });
     });
